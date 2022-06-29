3f5ef856f43f88a7cae65f16ba00b3b3a3983ecf
NoamZeise <noam4134@gmail.com>
2022-06-29 15:25:08 +0100

    texture render + vysnc

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::image;
+use sdl2::image::LoadTexture;
 use sdl2::rect::Rect;
 
 use std::path::Path;
-use std::time::Duration;
 
 pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
-
+    let _image_context = image::init(image::InitFlag::PNG);
     let window = video_subsystem
-        .window("rust-sdl2 demo: Video", 800, 600)
+        .window("ZL001", 1600, 900)
         .position_centered()
         .opengl()
         .build()
         .map_err(|e| e.to_string())?;
 
-    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    let mut canvas = window
+        .into_canvas()
+        .present_vsync()
+        .build()
+        .map_err(|e| e.to_string())?;
 
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
-    canvas.clear();
-    canvas.present();
+    let texture_creator = canvas.texture_creator();
+
+    canvas.set_draw_color(Color::RGB(100, 100, 100));
+
     let mut event_pump = sdl_context.event_pump()?;
+
+    let gaia_tex = texture_creator.load_texture(Path::new("textures/gaia.png"))?;
+
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -37,8 +46,21 @@ pub fn main() -> Result<(), String> {
         }
 
         canvas.clear();
+
+        canvas.copy(
+            &gaia_tex,
+            None,
+            Rect::new
+                (
+                    100,
+                    100,
+                    gaia_tex.query().width  * 4,
+                    gaia_tex.query().height * 4
+                )
+        )?;
+
         canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+
         // The rest of the game loop goes here...
     }
 
